3aaca3c4f7a7ee85a3b6b49649593c8c251725df
naxsyambis <naxsyambis@gmail.com>
2024-04-04 11:28:00 +0100

    membuat sistem menerima nomor yang akan dicari

--- AlgorithmaSearch_0163/AlgorithmaSearch_0163.cpp
+++ AlgorithmaSearch_0163/AlgorithmaSearch_0163.cpp
@@ -32,6 +32,16 @@ void LinearSearch()
     char ch;
     int ctr; // Number of comparisons
     int item;
+
+    do
+    {
+        // Accept the number to be searched
+        cout << "\nEnter the element you want to search: "; // step 1
+        cin >> item;
+
+        
+
+    } 
 }
 
 int main()
